96a80207cd128a14b040ec8cf2bcd108989e4bdf
jethro <mrhu2017web@gmail.com>
2025-07-03 17:32:10 +0800

    feat: 初始化项目结构并添加 TailwindCSS 和 Ant Design Vue
    - 修改 App.vue 文件，移除 NuxtWelcome 组件，添加 NuxtPage 组件
    - 新建 main.css 文件，引入 TailwindCSS 样式
    - 更新 nuxt.config.ts，添加 TailwindCSS 和 Ant Design Vue 配置
    - 更新 package.json，添加相关依赖
    - 新建 index.vue 和 user.vue 页面文件

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    123
+   <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-[red]">index</div>
+  <a-button>Button</a-button>
+</template>
+<script setup lang='ts'></script>
+<style scoped ></style>
